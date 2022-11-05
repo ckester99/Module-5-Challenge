7c5f54db3a588097679479dd67d8267223dfe3d2
Christian L. Kester <christiankester77@gmail.com>
2022-11-05 11:45:10 -0400

    added current date to header

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
 $(function () {
-    // TODO: Add a listener for click events on the save button. This code should
-    // use the id in the containing time-block as a key to save the user input in
-    // local storage. HINT: What does `this` reference in the click listener
-    // function? How can DOM traversal be used to get the "hour-x" id of the
-    // time-block containing the button that was clicked? How might the id be
-    // useful when saving the description in local storage?
-
     initTimeBlocks();
+
+    /*///////////////FUNCTION DEFINITIONS//////////////////////////////*/
 
     function initTimeBlocks() {
         var timeBlocks = $(".time-block");
@@ -45,13 +40,25 @@ $(function () {
         }
     }
 
+    function displayCurrentDay() {
+        var p = $("#currentDay");
+        var now = dayjs();
+
+        var suffix = "th";
+        var curDate = now.date();
+        if (curDate === 1) {
+            suffix = "st";
+        } else if (curDate === 2) {
+            suffix = "nd";
+        } else if (curDate === 3) {
+            suffix = "rd";
+        }
+
+        p.text(now.format("dddd, MMMM D") + suffix);
+    }
+
     setInterval(function () {
         applyTimeStyles();
+        displayCurrentDay();
     }, 1000);
-
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
-    //
-    // TODO: Add code to display the current date in the header of the page.
 });
